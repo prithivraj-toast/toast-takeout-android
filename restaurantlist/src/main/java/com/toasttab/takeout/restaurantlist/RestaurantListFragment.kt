d8f27fb10bdd3078dea54132e4177be0b1fd3209
prithivraj-toast <prithivraj.selvaraj@toasttab.com>
2019-11-03 22:02:56 +0000

    Added UI for restaurant list

--- restaurantlist/src/main/java/com/toasttab/takeout/restaurantlist/RestaurantListFragment.kt
+++ restaurantlist/src/main/java/com/toasttab/takeout/restaurantlist/RestaurantListFragment.kt
@@ -1,22 +1,23 @@
 package com.toasttab.takeout.restaurantlist
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_restaurant_list.*
 
 class RestaurantListFragment : Fragment() {
 
-    lateinit var viewModel: RestaurantListViewModel
+    private lateinit var viewModel: RestaurantListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_restaurant_list_item, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_restaurant_list, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -26,7 +27,35 @@ class RestaurantListFragment : Fragment() {
         )[RestaurantListViewModel::class.java]
 
         viewModel.viewState.observe(this, Observer {
-            Log.d("test", it.toString())
+            when (it) {
+                ContentState.Loading -> {
+                    progressBar.visibility = View.VISIBLE
+                    restaurant_list.visibility = View.GONE
+                    error_message.visibility = View.GONE
+                }
+                is ContentState.Content -> {
+                    progressBar.visibility = View.GONE
+                    restaurant_list.visibility = View.VISIBLE
+                    error_message.visibility = View.GONE
+
+                    val currentAdapter = restaurant_list.adapter
+                    if (currentAdapter == null) {
+                        restaurant_list.apply {
+                            layoutManager = LinearLayoutManager(context)
+                            adapter = RestaurantListRecyclerViewAdapter(it.data)
+                        }
+                    } else {
+                        (currentAdapter as RestaurantListRecyclerViewAdapter).setData(it.data)
+                        currentAdapter.notifyDataSetChanged()
+                    }
+                }
+                is ContentState.Error -> {
+                    progressBar.visibility = View.GONE
+                    restaurant_list.visibility = View.GONE
+                    error_message.visibility = View.VISIBLE
+                    error_message.text = it.errorMessage
+                }
+            }
         })
         viewModel.onViewCreated(
             42.3600807,
--- restaurantlist/src/main/java/com/toasttab/takeout/restaurantlist/RestaurantListRecyclerViewAdapter.kt
+++ restaurantlist/src/main/java/com/toasttab/takeout/restaurantlist/RestaurantListRecyclerViewAdapter.kt
@@ -1,16 +1,45 @@
 package com.toasttab.takeout.restaurantlist
 
-/*class RestaurantListRecyclerViewAdapter(items: List<RestaurantListModel>) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
+class RestaurantListRecyclerViewAdapter(private var items: List<RestaurantListModel>) :
+    RecyclerView.Adapter<RestaurantListViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestaurantListViewHolder =
+        RestaurantListViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.fragment_restaurant_list_item,
+                parent,
+                false
+            )
+        )
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: RestaurantListViewHolder, position: Int) {
+        val restaurant = items[position]
+        Picasso.get().load("https://s3.amazonaws.com/toasttab${restaurant.imageUrl}")
+            .into(holder.restaurantImage)
+        holder.restaurantName.text = restaurant.name
+        holder.restaurantDistance.text = restaurant.distance.toString()
+        holder.restaurantPrepTime.text = restaurant.minimumTakeoutTime.toString()
+        holder.restaurantType.text = restaurant.cuisineType
     }
 
-    override fun getItemCount(): Int {
-
+    fun setData(data: List<RestaurantListModel>) {
+        items = data
     }
+}
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
-    }
-}*/
+class RestaurantListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val restaurantImage: ImageView = itemView.findViewById(R.id.restaurant_image)
+    val restaurantName: TextView = itemView.findViewById(R.id.restaurant_name)
+    val restaurantDistance: TextView = itemView.findViewById(R.id.restaurant_distance)
+    val restaurantPrepTime: TextView = itemView.findViewById(R.id.restaurant_preptime)
+    val restaurantType: TextView = itemView.findViewById(R.id.restaurant_type)
+}
